87f4fa0fdbbe7512713652c2ddab773fabab6743
FrankLin <lin.xiaoe.f@gmail.com>
2016-12-26 17:11:34 +0800

    Add some rules using tslint-microsoft-contrib.

---
+++ tests/preferconstrule/preferconstrule.ts
@@ -0,0 +1,10 @@
+// Copyright 2016 Frank Lin (lin.xiaoe.f@gmail.com). All rights reserved.
+// Use of this source code is governed a license that can be found in the LICENSE file.
+
+/**
+ * This is js doc.
+ */
+
+function a() {
+  let x = 123;
+}

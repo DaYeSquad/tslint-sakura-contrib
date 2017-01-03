ce8153e1d7976276be1323cbd8051b6d92ef7bc2
FrankLin <lin.xiaoe.f@gmail.com>
2017-01-03 11:21:59 +0800

    Variable name should be camelCased or UPPER_CASED.

--- bin/test.sh
+++ bin/test.sh
@@ -4,4 +4,5 @@ BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$(dirname "$BIN_DIR")"
 
 #./node_modules/.bin/tslint -c ""${ROOT_DIR}"/tests/alllowercasefilenamerule/tslint.json" "tests/alllowercasefilenamerule/*.ts"
-./node_modules/.bin/tslint -c ""${ROOT_DIR}"/tslint.json" "tests/preferconstrule/*.ts"
+#./node_modules/.bin/tslint -c ""${ROOT_DIR}"/tslint.json" "tests/preferconstrule/*.ts"
+./node_modules/.bin/tslint -c ""${ROOT_DIR}"/tslint.json" "tests/variablenamerule/*.ts"
